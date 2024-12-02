cb00e8d253b10ae5663816d881ed4769cf8361cd
Ibrohim0056 <ibrohimjonathamjonov@gmail.com>
2024-12-02 14:55:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+ let body = document.querySelector("body")
+
+ let text = document.querySelector(".text2")
+
+ text.addEventListener("click", function(){
+    body.classList.toggle('white')
+ })
+
+
+
+
+ let userName = prompt("Ismingizni kiriting:");
+alert("Salom, " + userName + "!");
+
